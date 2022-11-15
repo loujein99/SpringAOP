f2863e913325f3c7287988954d694de86e15ee85
louay big <louay.benkasdallah@esprit.tn>
2022-11-15 23:33:20 +0100

    test secteur finish

---
+++ src/test/java/com/esprit/examen/services/SecteurServiceImplMockitoTest.java
@@ -0,0 +1,123 @@
+package com.esprit.examen.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.esprit.examen.entities.SecteurActivite;
+import com.esprit.examen.repositories.SecteurActiviteRepository;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class SecteurServiceImplMockitoTest {
+
+
+    @Mock
+    SecteurActiviteRepository sr;
+    @InjectMocks
+    SecteurActiviteServiceImpl ss;
+
+
+
+    SecteurActivite s= SecteurActivite.builder().codeSecteurActivite("111ee").libelleSecteurActivite("secteur test").build();
+    List<SecteurActivite> listSecteur = new ArrayList<SecteurActivite>() {
+
+        private static final long serialVersionUID = 1L;
+
+        {
+            add(SecteurActivite.builder().codeSecteurActivite("22b").libelleSecteurActivite("secteur test").build());
+            add(SecteurActivite.builder().codeSecteurActivite("333c").libelleSecteurActivite("secteur test").build());
+
+        }
+    };
+
+
+    @Test
+    public void testAddSecteur()
+    {
+        Mockito.when(sr.save(s)).thenReturn(s);
+        SecteurActivite secteuradd = ss.addSecteurActivite(s);
+        Assertions.assertNotNull(secteuradd);
+    }
+
+    @Test
+    public void testUpdateSecteur() {
+        s.setLibelleSecteurActivite("secteur aicha");
+        Mockito.when(sr.save(s)).thenReturn(s);
+        SecteurActivite sa = ss.updateSecteurActivite(s);
+        Assertions.assertEquals(s,sa);
+    }
+
+    @Test
+    public void RetrieveAllTest() {
+        List<SecteurActivite> s = new ArrayList<>();
+        s.add(new SecteurActivite());
+
+        when(sr.findAll()).thenReturn(s);
+
+        List<SecteurActivite> expected = ss.retrieveAllSecteurActivite();
+
+        //log.info("get ==> " + String.valueOf(s));
+
+        assertEquals(expected, s);
+        verify(sr).findAll();
+    }
+
+
+    @Test
+    public void DeleteSecteurTest() throws Exception{
+        SecteurActivite s = new SecteurActivite();
+        s.setCodeSecteurActivite("22buu");
+        s.setLibelleSecteurActivite("del aicha");
+
+        Mockito.when(sr.findById(s.getIdSecteurActivite())).thenReturn(Optional.of(s));
+        ss.deleteSecteurActivite(s.getIdSecteurActivite());
+        verify(sr).deleteById(s.getIdSecteurActivite());
+
+    }
+
+
+    @Test()
+    public void should_throw_exception_when_user_doesnt_exist()  {
+        SecteurActivite s = new SecteurActivite();
+        s.setCodeSecteurActivite("111111");
+        s.setLibelleSecteurActivite("secteur nouv");
+
+        given(sr.findById(anyLong())).willReturn(Optional.ofNullable(null));
+        ss.deleteSecteurActivite(s.getIdSecteurActivite());
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/esprit/examen/services/SecteurServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.esprit.examen.services;
+
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.esprit.examen.entities.SecteurActivite;
+
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+
+public class SecteurServiceImplTest {
+    @Autowired
+    ISecteurActiviteService secteurImpl;
+
+    @Test
+    @Order(1)
+    public void testSaveSecteur(){
+        SecteurActivite s= secteurImpl.addSecteurActivite(SecteurActivite.builder().codeSecteurActivite("22b").libelleSecteurActivite("secteur test").build());
+        Assertions.assertNotNull(s);
+    }
+
+
+   /* @Test
+    @Order(2)
+	public void testUpdateSecteur() {
+    	  SecteurActivite s= secteurImpl.addSecteurActivite(SecteurActivite.builder().codeSecteurActivite("22bcc").libelleSecteurActivite("secteur test").build());
+    	  s.setLibelleSecteurActivite("secteur aicha");
+	      Assertions.assertEquals(5,secteurImpl.updateSecteurActivite(s).getLibelleSecteurActivite());
+	    }*/
+
+
+}
